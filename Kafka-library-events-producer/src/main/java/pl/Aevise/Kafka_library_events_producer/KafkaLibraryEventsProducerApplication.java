a393ce371ac6d138878870607696e6e0ec73bf58
Mateusz Bartnicki <m.bartnicki38@gmail.com>
2024-08-01 19:46:22 +0200

    Initial commit

---
+++ Kafka-library-events-producer/src/main/java/pl/Aevise/Kafka_library_events_producer/KafkaLibraryEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package pl.Aevise.Kafka_library_events_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLibraryEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLibraryEventsProducerApplication.class, args);
+	}
+
+}
---
+++ Kafka-library-events-producer/src/test/java/pl/Aevise/Kafka_library_events_producer/KafkaLibraryEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Aevise.Kafka_library_events_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLibraryEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
