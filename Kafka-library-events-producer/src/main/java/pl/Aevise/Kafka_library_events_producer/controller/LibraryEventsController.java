f692bd7bfe90c367164acf0e13e29bfe8e271602
Mateusz Bartnicki <m.bartnicki38@gmail.com>
2024-08-03 20:17:56 +0200

    added library events controller with some example curl commands

---
+++ Kafka-library-events-producer/src/main/java/pl/Aevise/Kafka_library_events_producer/controller/LibraryEventsController.java
@@ -0,0 +1,28 @@
+package pl.Aevise.Kafka_library_events_producer.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.Aevise.Kafka_library_events_producer.domain.LibraryEvent;
+
+@RestController
+@Slf4j
+public class LibraryEventsController {
+
+    private static final String LIBRARY_EVENT = "/v1/libraryEvent";
+
+    @PostMapping("/v1/libraryEvent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(
+            @RequestBody LibraryEvent libraryEvent
+    ){
+        log.info("library event : {}", libraryEvent);
+        //invoke kafka producer
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(libraryEvent);
+    }
+}
