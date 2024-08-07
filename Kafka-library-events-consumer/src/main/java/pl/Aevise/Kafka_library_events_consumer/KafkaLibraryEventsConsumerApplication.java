62f95f3fba543fcc5055bdb871ed723d7fb2a75e
Mateusz Bartnicki <m.bartnicki38@gmail.com>
2024-08-07 19:59:41 +0200

    added library event consumer

---
+++ Kafka-library-events-consumer/src/main/java/pl/Aevise/Kafka_library_events_consumer/KafkaLibraryEventsConsumerApplication.java
@@ -0,0 +1,13 @@
+package pl.Aevise.Kafka_library_events_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLibraryEventsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLibraryEventsConsumerApplication.class, args);
+	}
+
+}
---
+++ Kafka-library-events-consumer/src/test/java/pl/Aevise/Kafka_library_events_consumer/KafkaLibraryEventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Aevise.Kafka_library_events_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLibraryEventsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
