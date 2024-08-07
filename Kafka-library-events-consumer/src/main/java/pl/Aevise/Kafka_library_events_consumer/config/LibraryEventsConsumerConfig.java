2c0a0690a1671aba8807ff4fe8f8a1555cc4b624
Mateusz Bartnicki <m.bartnicki38@gmail.com>
2024-08-07 20:45:56 +0200

    added kafka consumer configuration

---
+++ Kafka-library-events-consumer/src/main/java/pl/Aevise/Kafka_library_events_consumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package pl.Aevise.Kafka_library_events_consumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
+public class LibraryEventsConsumerConfig {
+}
