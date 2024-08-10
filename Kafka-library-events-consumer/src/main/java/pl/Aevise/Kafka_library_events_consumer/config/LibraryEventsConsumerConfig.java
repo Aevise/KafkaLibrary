f31ab87f635324d955424c63fbc113d9a5f89655
Mateusz Bartnicki <m.bartnicki38@gmail.com>
2024-08-10 19:23:04 +0200

    h2 database configuration

--- Kafka-library-events-consumer/src/main/java/pl/Aevise/Kafka_library_events_consumer/config/LibraryEventsConsumerConfig.java
+++ Kafka-library-events-consumer/src/main/java/pl/Aevise/Kafka_library_events_consumer/config/LibraryEventsConsumerConfig.java
@@ -6,28 +6,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 
 @EnableKafka
 @Configuration
 public class LibraryEventsConsumerConfig {
 
-//    sets manual acknowledgement
+    //    sets manual acknowledgement
 //    private static void setAcknowledgeModeToManual(ConcurrentKafkaListenerContainerFactory<Object, Object> factory) {
 //        ContainerProperties properties = factory.getContainerProperties();
 //        properties.setAckMode(ContainerProperties.AckMode.MANUAL);
 //    }
 //
-//    @Bean
-//    ConcurrentKafkaListenerContainerFactory<?, ?> concurrentKafkaListenerContainerFactory(
-//            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
-//            ConsumerFactory<Object, Object> kafkaConsumerFactory
-//    ) {
-//        var factory = new ConcurrentKafkaListenerContainerFactory<>();
-//        configurer.configure(factory, kafkaConsumerFactory);
-//
+    static int NUMBER_OF_THREADS_FOR_KAFKA_LISTENER = 3;
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> kafkaConsumerFactory
+    ) {
+
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+        factory.setConcurrency(NUMBER_OF_THREADS_FOR_KAFKA_LISTENER);
+
 //        setAcknowledgeModeToManual(factory);
-//
-//        return factory;
-//    }
+
+        return factory;
+    }
 }
